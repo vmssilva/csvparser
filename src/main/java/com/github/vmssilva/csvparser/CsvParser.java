2a418d90a09453efed3a2856614ed152cffcbe8c
dida <vsilva.ad@gmail.com>
2026-03-08 15:14:41 -0300

    added parser and tests

--- src/main/java/com/github/vmssilva/csvparser/CsvParser.java
+++ src/main/java/com/github/vmssilva/csvparser/CsvParser.java
@@ -3,79 +3,191 @@ package com.github.vmssilva.csvparser;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import com.github.vmssilva.csvparser.config.CsvConfig;
+import com.github.vmssilva.csvparser.exception.CsvParseException;
 import com.github.vmssilva.csvparser.model.CsvDocument;
 import com.github.vmssilva.csvparser.model.CsvRow;
-import com.github.vmssilva.csvparser.model.Document;
 import com.github.vmssilva.csvparser.model.Row;
 
 public class CsvParser {
 
-  public Document parse(InputStream inputStream) {
-    return null;
+  private CsvConfig config;
+  private String csv;
+  private List<String> cols;
+  private List<Row> rows;
+  private int current;
+  private StringBuilder value;
+
+  public CsvParser(CsvConfig config) {
+    this.config = Objects.requireNonNull(config);
   }
 
-  public Document parse(InputStream inputStream, CsvConfig config) {
-    return null;
+  public CsvParser() {
+    this(new CsvConfig());
   }
 
-  public Document parse(String csv) {
-    return parse(csv, new CsvConfig());
+  public CsvDocument parse(InputStream inputStream) {
+    throw new UnsupportedOperationException("Not implemented yet");
   }
 
-  public Document parse(String csv, CsvConfig config) {
-    List<Row> rows = new ArrayList<>();
-    List<String> cols = new ArrayList<>();
-    StringBuilder value = new StringBuilder();
-    char delimiter = config.getDelimiter();
-    char lineBreak = config.getLineBreakCharacter();
-    char quoteCharacter = config.getQuoteCharacter();
+  public CsvDocument parse(String source) {
+    this.current = 0;
+    this.value = new StringBuilder();
+    this.cols = new ArrayList<>();
+    this.rows = new ArrayList<>();
+    this.csv = source;
+
     boolean hasHeader = config.hasHeader();
 
-    int current = 0;
+    while (!(isAtEnd())) {
 
-    csv = csv
-        .trim()
-        .replace("\t\n+", "\n")
-        .replace("\n+", "\n")
-        .replace("^\n+", "")
-        .replace("\n+$", "")
-        .trim();
+      // return true if there's any character to consume
+      if (scan(peek()))
+        value.append(consume());
 
-    while (current < csv.length()) {
-      char c = csv.charAt(current++);
-      value.append(c);
+      if (isAtEnd())
+        updateRows();
 
-      if (c == '\\') {
-        // space to handle escape charater
-      }
-
-      if (c == quoteCharacter) {
-        // space to handle quote charater
-      }
-
-      if (c == delimiter || c == lineBreak) {
-        // Ensure that the delimiter or linebreak character is not added to the cols
-        cols.add(value.toString().substring(0, value.length() - 1));
-        // cleaning the value string to the next iteration
-        value.setLength(0);
-      }
-
-      if (c == lineBreak) {
-        rows.add(new CsvRow(cols));
-        // cleaning the cols to the next iteration
-        cols = new ArrayList<>();
-      }
-
-      if (current >= csv.length()) {
-        // adding last field to cols
-        cols.add(value.toString());
-        rows.add(new CsvRow(cols));
-      }
     }
 
     return new CsvDocument(rows, hasHeader);
   }
 
+  private boolean scan(char c) {
+
+    char delimiter = this.config.getDelimiter();
+    char newLine = this.config.getNewLineCharacter();
+    char quotes = this.config.getQuoteCharacter();
+
+    if (c == '\\')
+      return handleEscapes();
+
+    if (c == quotes)
+      return handleQuotes();
+
+    if (c == delimiter)
+      return handleDelimiter();
+
+    if (c == newLine)
+      return handleNewLine();
+
+    return true;
+  }
+
+  private boolean handleDelimiter() {
+    cols.add(value.toString());
+    value.setLength(0);
+    consume();
+    return false;
+  }
+
+  private boolean handleNewLine() {
+    updateRows();
+    consume();
+    return false;
+  }
+
+  private boolean handleQuotes() {
+
+    char quotes = this.config.getQuoteCharacter();
+    int started = current;
+    boolean inQuotes = true;
+
+    // consume quotes
+    consume();
+
+    while (!isAtEnd()) {
+
+      if (!inQuotes)
+        break;
+
+      char c = consume();
+
+      if (c == quotes) {
+        started = current;
+        inQuotes = !inQuotes;
+        continue;
+      }
+
+      if (inQuotes) {
+        value.append(c);
+        continue;
+      }
+
+    }
+
+    if (inQuotes)
+      throw new CsvParseException("Unclosed string at index: " + started);
+
+    return false;
+  }
+
+  private boolean handleEscapes() {
+    if (isAtEnd())
+      throw new CsvParseException("Invalid escape character found at index: " +
+          current);
+
+    // consume backslash character
+    consume();
+    char escaped = peek();
+
+    switch (escaped) {
+      case '\\' -> value.append('\\');
+      case '"' -> value.append('"');
+      case 't' -> value.append('\t');
+      case 'f' -> value.append('\f');
+      case 'b' -> value.append('\b');
+      case 'n' -> value.append('\n');
+      case '0' -> value.append('\0');
+      case 'u' -> {
+
+        int begin = current + 1;
+        int end = begin + 4;
+
+        if (end > csv.length())
+          throw new CsvParseException("Invalid unicode character EOF");
+
+        String unicodeStr = csv.substring(begin, end);
+
+        try {
+          char unicode = (char) Integer.parseInt(unicodeStr, 16);
+          value.append(unicode);
+
+          current += (end - begin);
+
+        } catch (Exception e) {
+          throw new CsvParseException("Invalid unicode character '\\u" + unicodeStr + "'");
+        }
+      }
+      default ->
+        throw new CsvParseException("Invalid escape character '\\" + escaped + "' found at index: " + current);
+    }
+
+    // consume escape character
+    consume();
+
+    return false;
+
+  }
+
+  private char consume() {
+    return (isAtEnd()) ? '\0' : csv.charAt(current++);
+  }
+
+  private char peek() {
+    return (isAtEnd()) ? '\0' : csv.charAt(current);
+  }
+
+  private boolean isAtEnd() {
+    return current >= csv.length();
+  }
+
+  private void updateRows() {
+    cols.add(value.toString());
+    rows.add(new CsvRow(cols));
+    value.setLength(0);
+    cols = new ArrayList<>();
+  }
 }
---
+++ src/test/java/com/github/vmssilva/csvparser/CsvParserTest.java
@@ -0,0 +1,159 @@
+package com.github.vmssilva.csvparser;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.github.vmssilva.csvparser.config.CsvConfig;
+import com.github.vmssilva.csvparser.exception.CsvParseException;
+import com.github.vmssilva.csvparser.model.CsvDocument;
+
+public class CsvParserTest {
+
+  @Test
+  void shouldHandleEmptyCsv() {
+    CsvDocument doc = parse("");
+
+    assertEquals(0, doc.rows().size());
+    assertEquals(Optional.empty(), doc.getHeader());
+  }
+
+  @Test
+  void shouldHandleCsvWithOnlyHeader() {
+    CsvConfig config = new CsvConfig();
+    config.setHasHeader(true);
+
+    CsvDocument doc = parse("Id,Name,Salary", config);
+
+    assertEquals(0, doc.rows().size());
+    assertEquals(List.of("Id", "Name", "Salary"), doc.getHeader().get().values());
+  }
+
+  @Test
+  @DisplayName("Should be able to parse a csv with single line")
+  void testParserWithSingleLine() {
+    var csv = "Id,Name,Position,Salary";
+    CsvDocument doc = parse(csv);
+    assertEquals(List.of("Id", "Name", "Position", "Salary"), doc.rows().get(0).values());
+
+  }
+
+  @Test
+  @DisplayName("Should be able to parse a csv with multiple lines")
+  void testParserWithMultiplesLines() {
+    var csv = """
+        Id,Name,Position,Salary
+        1,John Doe,Java Developer,5000.00""";
+
+    CsvDocument doc = parse(csv);
+
+    assertEquals(4, doc.rows().get(0).values().size());
+    assertEquals(List.of("Id", "Name", "Position", "Salary"),
+        doc.rows().get(0).values());
+    assertEquals(List.of("1", "John Doe", "Java Developer", "5000.00"),
+        doc.rows().get(1).values());
+  }
+
+  @Test
+  @DisplayName("Should be able to parse a csv with quotes at the start")
+  void testParserWithQuotesAtTheStart() {
+
+    var csv = "\"Id,Name\",Category";
+
+    CsvDocument doc = parse(csv);
+
+    assertEquals(List.of("Id,Name", "Category"),
+        doc.rows().get(0).values());
+  }
+
+  @Test
+  @DisplayName("Should be able to parse a csv with quotes at the middle")
+  void testParserWithQuotesAtTheMiddle() {
+
+    var expected = List.of("Id", "Name,Category", "Price");
+    CsvDocument doc = parse("Id,\"Name,Category\",Price");
+
+    assertEquals(expected, doc.rows().get(0).values());
+  }
+
+  @Test
+  @DisplayName("Should be able to parse a csv with quotes at the end")
+  void testParserWithQuotesAtTheEnd() {
+    var expected = List.of("Id", "Name", "Category,Price");
+    CsvDocument doc = parse("Id,Name,\"Category,Price\"");
+    assertEquals(expected, doc.rows().get(0).values());
+  }
+
+  @Test
+  @DisplayName("Should be able to parse a csv with multiple quotes")
+  void testParserWithQuotesInMultipleLines() {
+    var expected = List.of(
+        List.of("Id", "Name", "Category", "Price"),
+        List.of("1", "Smartphone, Samsung Galaxy, S22", "Electronics", "399.90"));
+
+    CsvDocument doc = parse("Id,Name,Category,Price\n1,\"Smartphone, Samsung\" \"Galaxy, S22\",Electronics,399.90");
+    assertEquals(expected.get(1), doc.rows().get(1).values());
+  }
+
+  @Test
+  @DisplayName("Should be able to parse csv with escapes characters")
+  void testParserWithEscapesCharacters() {
+
+    var csv = "Id,Name \\\"Description\\\",Price";
+    var expected = List.of("Id", "Name \"Description\"", "Price");
+    CsvDocument doc = parse(csv);
+
+    assertEquals(expected, doc.rows().get(0).values());
+
+  }
+
+  @Test
+  @DisplayName("Should be able to parse csv with multiple escape characters")
+  void testParserWithMultiplesEscapesCharacters() {
+
+    var csv = "1,Smartphone \\\\Samsung \\\"refurbished\\\",Electronics,50.00";
+    var expected = List.of("1", "Smartphone \\Samsung \"refurbished\"", "Electronics", "50.00");
+    CsvDocument doc = parse(csv);
+
+    assertEquals(expected, doc.rows().get(0).values());
+  }
+
+  @Test
+  @DisplayName("Should be able to parse csv with unicode escape characters")
+  void testParserWithUnicodeCharacter() {
+
+    var csv = "1,Smartphone Samsung,Electronics,\\u20AC50.00"; // Euro sign
+    var expected = List.of("1", "Smartphone Samsung", "Electronics", "\u20AC50.00");
+    CsvDocument doc = parse(csv);
+    assertEquals(expected, doc.rows().get(0).values());
+  }
+
+  @Test
+  @DisplayName("Should throw CsvParseException for invalid escape characters")
+  void testParseWithInvalidEscapeCharacters() {
+    Throwable t = assertThrows(CsvParseException.class, () -> parse("\\u002x").rows().get(0).values());
+    assertTrue(t.getMessage().contains("Invalid unicode character"));
+  }
+
+  @Test
+  @DisplayName("Should throw CsvParseException for unclosed strings")
+  void testParseWithUnclosedQuotes() {
+
+    Throwable t = assertThrows(CsvParseException.class, () -> parse("Id,\"Name,Price"));
+    assertTrue(t.getMessage().contains("Unclosed string"));
+  }
+
+  private CsvDocument parse(String csv) {
+    return new CsvParser().parse(csv);
+  }
+
+  private CsvDocument parse(String csv, CsvConfig config) {
+    return new CsvParser(config).parse(csv);
+  }
+}
